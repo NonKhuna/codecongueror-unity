3c0dde5e45743f0c6d46e888fae1089c429e68ab
NonKhuna <khunanon.rtgs@gmail.com>
2021-08-01 21:11:50 +0700

    :tada:  init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import Unity, { UnityContext } from "react-unity-webgl";
+import React, { Fragment, useEffect, useState } from "react";
+
+const unityContext = new UnityContext({
+  loaderUrl: "build/app.loader.js",
+  dataUrl: "build/app.data",
+  frameworkUrl: "build/app.framework.js",
+  codeUrl: "build/app.wasm",
+});
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [isLoaded, setLoaded] = useState(false);
+    const [herohealth, setHeroHealth] = useState(100);
+    const [enemyhealth, setEnemyHealth] = useState(100);
+    const demoData = {name:"non", handsome:100, damage:10};
+
+    function tricker1() {
+        unityContext.send("GameController", "TrickEnemyButton1");
+    }
+    function tricker2() {
+        unityContext.send("GameController", "TrickEnemyButton2");
+    }
+    function tricker3() {
+        unityContext.send("GameController", "TrickEnemyButton3");
+    }
+
+    function initial() {
+        unityContext.send("GameController", "receiveData", JSON.stringify(demoData));
+    }
+
+    useEffect(function(){
+        unityContext.on("HeroHealth", function (hp) {
+            setHeroHealth(hp);
+        });
+        unityContext.on("EnemyHealth", function (hp) {
+            setEnemyHealth(hp);
+        });
+    }, []);
+
+    return <Fragment>
+        <h1>React Unity WebGL Test</h1>
+        <button onClick={tricker1}>tricker1</button>
+        <button onClick={tricker2}>tricker2</button>
+        <button onClick={tricker3}>tricker3</button>
+        <button onClick={initial}>sendData</button>
+        <p>Hero health = {herohealth}</p>
+        <p>Enemy health = {enemyhealth}</p>
+        <Unity 
+            unityContext={unityContext}
+            style={{
+                width: 544,
+                height: 306,
+                border: "2px solid black",
+                background: "grey",
+              }}
+        />
+    </Fragment>
 }
 
 export default App;
